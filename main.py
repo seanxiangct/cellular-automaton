97d91eace8c2fcd3651be94005598c746c4f0426
Sean Xiang <sean.xiang@sydney.edu.au>
2022-08-14 12:27:23 +1000

    initial refactoring complete

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ fps = 10
 # colours
 
 # session variables
-start = False
 win = None
 # n_cols = s_width // cell_size
 # n_rows = s_height // cell_size
@@ -22,34 +21,34 @@ rule = []
 # current_grid = []
 # new_grid = []
 
-def activate_cell(pix_x, pix_y, env):
-    # find the nearest cell on the grid
+# def activate_cell(pix_x, pix_y, env):
+#     # find the nearest cell on the grid
 
-    # TODO: optimization
-    for row in env:
-        for cell in row:
-            if cell.collidepoint(pix_x, pix_y):
-                cell.state = 1
-                pygame.draw.rect(win, Colours.green_clr, cell)
+#     # TODO: optimization
+#     for row in env:
+#         for cell in row:
+#             if cell.collidepoint(pix_x, pix_y):
+#                 cell.state = 1
+#                 pygame.draw.rect(win, Colours.green_clr, cell)
 
-                break
+#                 break
 
-def update_grid():
-    global current_grid
-    for row in current_grid:
-        for cell in row:
-            neighbours = cell.get_neighbours(current_grid)
-            life_count = 0
-            for n in neighbours:
-                if n.state == 1:
-                    life_count += 1
-            new_state = cell.update(life_count)
-            new_grid[cell.i_x][cell.i_y].state = new_state
-            if new_state:
-                pygame.draw.rect(win, Colours.green_clr, cell)
-            else:
-                pygame.draw.rect(win, Colours.white_clr, cell)
-    current_grid = new_grid
+# def update_grid():
+#     # global current_grid
+#     for row in current_grid:
+#         for cell in row:
+#             neighbours = cell.get_neighbours(current_grid)
+#             life_count = 0
+#             for n in neighbours:
+#                 if n.state == 1:
+#                     life_count += 1
+#             new_state = cell.update(life_count)
+#             new_grid[cell.i_x][cell.i_y].state = new_state
+#             if new_state:
+#                 pygame.draw.rect(win, Colours.green_clr, cell)
+#             else:
+#                 pygame.draw.rect(win, Colours.white_clr, cell)
+#     current_grid = new_grid
 
 
 def test():
@@ -69,19 +68,17 @@ def init(scene: Scene):
 
     # win.fill(Colours.white_clr)
 
-    global current_grid
-    global new_grid
+    # global current_grid
+    # global new_grid
     # current_grid = draw_grid()
     # new_grid = draw_grid()
     scene = Scene(s_width, s_height)
     return scene
 
 
-
 def run(scene: Scene):
 
-    global start
-    global current_grid
+    start = False
     while run:
 
         # print(clock.get_fps())
@@ -102,13 +99,14 @@ def run(scene: Scene):
                     return
                 if event.key == pygame.K_SPACE:
                     start = True
-                    update_grid()
+                    scene.grid.update_grid()
 
                 if event.key == pygame.K_BACKSPACE:
                     # clear the screen and grid
-                    current_grid = scene.grid.draw_grid()
+                    # current_grid = scene.grid
+                    scene.grid.draw_grid()
                     start = False
-                    pass
+                    continue
 
             if pygame.mouse.get_focused():
                 # if mouse is held down
@@ -116,11 +114,11 @@ def run(scene: Scene):
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 left, _, _ = pygame.mouse.get_pressed()
                 if left:
-                    activate_cell(mouse_x, mouse_y, current_grid)
+                    scene.grid.activate_cell(mouse_x, mouse_y)
 
         # compute new grid based on current grid
         if start:
-            update_grid()
+            scene.grid.update_grid()
 
         pygame.display.update()
 
@@ -133,6 +131,5 @@ if __name__ == '__main__':
 
     s_width, s_height = 1200, 1200
     scene = Scene(s_width, s_height)
-    scene.grid.draw_grid()
     run(scene=scene)
 
--- models/Grid.py
+++ models/Grid.py
@@ -5,11 +5,12 @@ from .cell import Cell
 from statics.cellStates import CellStates
 from statics.colours import Colours
 
-# TODO: fix importshttps://stackoverflow.com/questions/14132789/relative-imports-for-the-billionth-time/14132912#14132912
-
 class Grid: 
     
     def __init__(self, display, width, height, cell_size=10, death_rate=0.05, gen_grid=False):
+        '''
+        populate grid with cells
+        '''
         self.display = display
         self.width = width
         self.height = height
@@ -18,11 +19,11 @@ class Grid:
         self._n_rows = self.height // self.cell_size
         self.death_rate = death_rate
         self.gen_grid = gen_grid
-        self.grid = self.draw_grid()
+        # self.grid = self.draw_grid()
+        self._grid = []
 
     def draw_grid(self):
 
-        grid = []
         x_cor = 0
         y_cor = 0
         for j in range(self._n_cols):
@@ -36,5 +37,35 @@ class Grid:
                     cell = Cell(x_cor + j * self.cell_size, y_cor + i * self.cell_size, self.cell_size, self.death_rate, 0)
                 row.append(cell)
                 pygame.draw.rect(self.display, Colours.white_clr, cell)
-            grid.append(row)
-        return grid
+            self._grid.append(row)
+        return self._grid
+
+    def update_grid(self):
+
+        # TODO: optimization
+        new_grid = self._grid
+        for row in self._grid:
+            for cell in row:
+                neighbours = cell.get_neighbours(self._grid)
+                life_count = 0
+                for n in neighbours:
+                    if n.state == 1:
+                        life_count += 1
+                new_state = cell.update(life_count)
+                new_grid[cell.i_x][cell.i_y].state = new_state
+                if new_state:
+                    pygame.draw.rect(self.display, Colours.green_clr, cell)
+                else:
+                    pygame.draw.rect(self.display, Colours.white_clr, cell)
+        self._grid = new_grid
+        return self._grid
+    
+    def activate_cell(self, pix_x, pix_y):
+    # find the nearest cell on the grid
+        for row in self._grid:
+            for cell in row:
+                if cell.collidepoint(pix_x, pix_y):
+                    cell.state = 1
+                    pygame.draw.rect(self.display, Colours.green_clr, cell)
+
+                    break
